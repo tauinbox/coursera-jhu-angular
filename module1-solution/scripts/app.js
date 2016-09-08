cd4ceaa0ea595b27c732b42c2336c1b88cdfed57
Alexander Tupavov <tauinbox@gmail.com>
2016-09-08 16:04:21 +0300

    Implement 1 assignment bonus

--- module1-solution/scripts/app.js
+++ module1-solution/scripts/app.js
@@ -1,24 +1,33 @@
 (function(){
   'use strict';
 
-  angular.module('checkApp', [])
+  angular.module('LunchCheck', [])
 
-  .controller('InputController', ['$scope', function($scope) {
+  // I chose this method of protection from minification
+  .controller('LunchCheckController', ['$scope', function($scope) {
 
     $scope.string = "";
     $scope.result = "";
+    $scope.color = "black";
+    $scope.bordercolor = "#cccccc";
 
     $scope.checkInput = function() {
       $scope.string = $scope.string.trim();
       if ($scope.string == "") {
         $scope.result = "Please enter data first";
+        $scope.color = "red";
+        $scope.bordercolor = "red";
         return;
       }
       var numberOfItems = $scope.string.split(',').length;
       if (numberOfItems > 3) {
         $scope.result = "Too much!";
+        $scope.color = "green";
+        $scope.bordercolor = "green";
       } else {
         $scope.result = "Enjoy!";
+        $scope.color = "green";
+        $scope.bordercolor = "green";
       }
     };
 
